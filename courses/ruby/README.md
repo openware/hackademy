0e1012d7de895c70f0d1f025f46a13e6ee43d5ee
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2019-08-30 02:03:15 +0300

    Add ex04-myattr to ruby module with my_attr_reader part (#7)

--- courses/ruby/README.md
+++ courses/ruby/README.md
@@ -29,13 +29,31 @@ ruby basics understanding.
 Don't forget to install dependencies with bundler `bundle install` before
 starting to work on tasks.
 
-List of exercises:
+#### List of exercises:
 
 - [ex01-enumerables](./ex01-enumerables)
 - [ex02-strings](./ex02-strings)
+- [ex03-wallet](./ex03-wallet)
 
-
-Resources:
+##### Resources:
 
 - [Enumerable](https://ruby-doc.org/core-2.6.3/Enumerable.html)
 - [String](https://ruby-doc.org/core-2.4.0/String.html)
+
+## Just metaprogram it
+
+This section was designed for understanding basics of one of the most impressive
+aspect of ruby language. Metaprogramming is the writing of computer programs 
+that write or manipulate other programs (or themselves) as their data, or that 
+do part of the work at compile time that would otherwise be done at runtime.
+
+#### List of exercises:
+
+- [ex04-myattr](./ex04-myattr)
+
+##### Resources:
+
+- [Don't Know Metaprogramming In Ruby?](http://rubylearning.com/blog/2010/11/23/dont-know-metaprogramming-in-ruby/#what-is-metaprogramming:18bee3e64eb657db6a08ae0ff05e44b8)
+- [How to Use attr_accessor, attr_writer & attr_reader](https://www.rubyguides.com/2018/11/attr_accessor/)
+- [Include vs Prepend vs Extend](https://medium.com/@leo_hetsch/ruby-modules-include-vs-prepend-vs-extend-f09837a5b073)
+- [Module define_method](https://apidock.com/ruby/Module/define_method)
---
+++ courses/ruby/ex04-myattr/my_attr_reader_test.rb
@@ -0,0 +1,124 @@
+require "minitest/autorun"
+require "minitest/pride"
+
+require_relative "my_attr_reader"
+
+class MyAttrReaderTest < Minitest::Test
+  def test_single_my_attr_reader
+    skip
+    klass = Class.new do
+      my_attr_reader :readable
+
+      def initialize
+        @readable = "readme"
+      end
+    end
+
+    instance = klass.new
+    assert_equal "readme", instance.readable
+  end
+
+  def test_multiple_my_attr_readers
+    skip
+    klass = Class.new do
+      my_attr_reader :readable1, :readable2
+      my_attr_reader :readable3
+      def initialize
+        @readable1 = "readme1"
+        @readable2 = "readme2"
+        @readable3 = "readme3"
+      end
+    end
+
+    instance = klass.new
+
+    assert_equal "readme1", instance.readable1
+    assert_equal "readme2", instance.readable2
+    assert_equal "readme3", instance.readable3
+  end
+
+  def test_duplicated_my_attr_reader
+    skip
+    klass = Class.new do
+      my_attr_reader :readable, :readable
+      my_attr_reader :readable
+      def initialize
+        @readable = "readme"
+      end
+    end
+
+    instance = klass.new
+    assert_equal "readme", instance.readable
+  end
+
+  def test_my_attr_reader_initialization
+    skip
+    klass = Class.new do
+      my_attr_reader :readable
+
+      def initialize(readable)
+        @readable = readable
+      end
+    end
+
+    instance = klass.new("readme")
+    assert_equal "readme", instance.readable
+  end
+
+  def test_my_attr_reader_changes
+    skip
+    klass = Class.new do
+      my_attr_reader :readable
+
+      def initialize
+        @readable = "readme"
+      end
+    end
+
+    instance = klass.new
+    assert_equal "readme", instance.readable
+
+    instance.instance_variable_set(:@readable, "readme one more time")
+    assert_equal "readme one more time", instance.readable
+  end
+
+  def test_my_attr_reader_with_custom_writer
+    skip
+    klass = Class.new do
+      my_attr_reader :readable
+
+      def initialize
+        @readable = "readme"
+      end
+
+      def readable=(r)
+        @readable = r
+      end
+    end
+
+    instance = klass.new
+    assert_equal "readme", instance.readable
+
+    instance.readable = "readme one more time"
+    assert_equal "readme one more time", instance.readable
+  end
+
+  def test_my_attr_reader_instance_methods
+    skip
+    klass = Class.new do
+      my_attr_reader :readable1, :readable2
+      my_attr_reader :readable3
+      def initialize
+        @readable1 = "readme1"
+        @readable2 = "readme2"
+        @readable3 = "readme3"
+      end
+    end
+
+    new_class_methods = klass.instance_methods - klass.superclass.instance_methods
+
+    # Check that only expected methods were defined.
+    assert_equal %i[readable1 readable2 readable3] - new_class_methods, []
+  end
+end
+
