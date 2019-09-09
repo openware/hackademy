3fe509386f392d39908034d413ea2f78ccdb1cb6
Khlopiachyi Pavlo <khlpavlo@gmail.com>
2019-09-09 11:57:21 +0300

    Add recursive search of value

--- courses/ruby/ex07/ex07.rb
+++ courses/ruby/ex07/ex07.rb
@@ -2,7 +2,6 @@ require 'yaml'
 require 'singleton'
 
 module MyApp
-  
   def config
     Config.instance
   end
@@ -10,9 +9,7 @@ module MyApp
   module_function :config
 
   class Config
-    
     include Singleton
-    
     def initialize
       @config = YAML.load_file("config.yml")
     end
@@ -20,19 +17,26 @@ module MyApp
     def out
       @config
     end
-    
     def method_missing(m)
       res = nil
+      find = m.to_s
       @config.each do |key, value|
-        value.each do |arg|
-          arg.each do |k, v|
-            if k == m.to_s
-              res = v
-              return res
-            end
-          end
+        if key == ENV["ENV_VAR"]
+          value.is_a?(Hash) ? (return search(value, find)) : (retrun "Error")
         end
+      end
+    end
+
+    def search(node, find)
+      if node.is_a?(Hash)
+        node.each do |key, value|
+          key == find ? (return value) : (return search(value, find) if value.is_a?(Hash))
+        end
+        nil
       end
     end
   end
 end
+
+test = MyApp.config
+puts test.other
