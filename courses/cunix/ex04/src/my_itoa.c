a9bb67f324b7e58b83b41150dd28b041828fd945
maxpushka <maxpushka200@gmail.com>
2021-08-08 21:10:12 +0300

    refactored ex04

--- courses/cunix/ex04/src/my_itoa.c
+++ courses/cunix/ex04/src/my_itoa.c
@@ -1,13 +1,14 @@
 #include "test.h"
 #include <malloc.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* converts integer into string */
 char *my_itoa(int nmb)
 {
     int is_negative = nmb < 0 ? 1 : 0;
     nmb = abs(nmb);
-    int n = 0;  // length of nmb (chars)
+    size_t n = 0;  // length of nmb (chars)
 
     int nmb_copy = nmb;
     do
@@ -17,20 +18,24 @@ char *my_itoa(int nmb)
     }
     while (nmb_copy > 0);
 
-    char *res = (char *) malloc(is_negative ? n + 1 : n);
-
-    int shift = 0;
-    if (is_negative)
-    {
-        res[0] = '-';
-        shift = 1;
-    }
+    char *res = (char *) malloc(n + 1);
+    res[n] = '\0';
 
     while (n > 0)
     {
-        res[n - 1 + shift] = nmb % 10 + '0';
+        res[n - 1] = nmb % 10 + '0';
         nmb /= 10;
         n--;
+    }
+
+    if (is_negative)
+    {
+        char *negative_res = (char *) malloc(strlen(res) + 2);
+        strcpy(negative_res, "-");
+        strcat(negative_res, res);
+
+        free(res);
+        return negative_res;
     }
 
     return res;
