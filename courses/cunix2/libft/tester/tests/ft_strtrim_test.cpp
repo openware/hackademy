9f067d1ea9f9a52e98e1882aeba17b837f0f7212
Kohelbekker <kohelbekker@gmail.com>
2021-08-09 08:41:24 +0300

    Initiazlie cunix2 module

---
+++ courses/cunix2/libft/tester/tests/ft_strtrim_test.cpp
@@ -0,0 +1,48 @@
+extern "C"
+{
+#define new tripouille
+#include "libft.h"
+#undef new
+}
+
+#include "sigsegv.hpp"
+#include "check.hpp"
+#include "leaks.hpp"
+#include <string.h>
+
+int iTest = 1;
+int main(void)
+{
+	signal(SIGSEGV, sigsegv);
+	title("ft_strtrim\t: ")
+
+	char * s = ft_strtrim("   tripouille");
+	/* 1 */ check(!strcmp(s, "tripouille"));
+	/* 2 */ mcheck(s, strlen("tripouille") + 1); free(s); showLeaks();
+
+	s = ft_strtrim("tripouille   ");
+	/* 3 */ check(!strcmp(s, "tripouille"));
+	/* 4 */ mcheck(s, strlen("tripouille") + 1); free(s); showLeaks();
+
+	s = ft_strtrim("   tripouille   ");
+	/* 5 */ check(!strcmp(s, "tripouille"));
+	/* 6 */ mcheck(s, strlen("tripouille") + 1); free(s); showLeaks();
+	
+	s = ft_strtrim("      ");
+	/* 7 */ check(!strcmp(s, ""));
+	/* 8 */ mcheck(s, 1); free(s); showLeaks();
+
+	s = ft_strtrim("");
+	/* 9 */ check(!strcmp(s, ""));
+	/* 10 */ mcheck(s, 1); free(s); showLeaks();
+
+	s = ft_strtrim("123");
+	/* 11 */ check(!strcmp(s, "123"));
+	/* 12 */ mcheck(s, 4); free(s); showLeaks();
+
+	s = ft_strtrim(" 12  34   ");
+	/* 13 */ check(!strcmp(s, "12  34"));
+	/* 14 */ mcheck(s, 1); free(s); showLeaks();
+	write(1, "\n", 1);
+	return (0);
+}
