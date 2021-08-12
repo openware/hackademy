164d179698907712bef4987b56483f41d20c25be
Kohelbekker <kohelbekker@gmail.com>
2021-08-12 16:05:30 +0300

    Add testing library for ft_printf

---
+++ courses/cunix2/ft_printf/printf_test/tests/tests.h
@@ -0,0 +1,25 @@
+#ifndef FT_PRINTF_TEST_TESTS_H
+# define FT_PRINTF_TEST_TESTS_H
+
+void test_pft_nacked(void);
+void test_pft_percent(void);
+void test_pft_nocrash(void);
+void test_pft_string(void);
+void test_pft_int_i(void);
+
+void test_moulitest_00(void);
+void test_moulitest_01(void);
+void test_moulitest_02(void);
+void test_moulitest_03(void);
+void test_moulitest_05(void);
+void test_moulitest_09(void);
+void test_moulitest_16(void);
+void test_moulitest_50(void);
+void test_moulitest_51(void);
+void test_moulitest_52(void);
+void test_moulitest_61(void);
+void test_moulitest_69(void);
+
+void test_printf_tests_test(void);
+
+#endif
