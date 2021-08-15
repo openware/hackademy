7b8c3e78d36a9edff496b4cc0d7ddb9c596bd22e
Anton Filonenko <filonenko100075@gmail.com>
2021-08-15 18:40:14 +0300

    Fix memccpy and strtrim tests

--- courses/cunix2/libft/tester/tests/ft_memccpy_test.cpp
+++ courses/cunix2/libft/tester/tests/ft_memccpy_test.cpp
@@ -20,9 +20,7 @@ int main(void)
 	memset(dest, 'A', 100);
 	/* 1 */ check(ft_memccpy(dest, "coucou", 0, 0) == 0 && dest[0] == 'A'); showLeaks();
 	/* 2 */ check(ft_memccpy(dest, "coucou", 0, 1) == 0 && dest[0] == 'c' && dest[1] == 'A'); showLeaks();
-	dest[0] = 'A';
 	/* 3 */ check(ft_memccpy(dest, "coucou", 'c', 10) == dest + 1 && dest[0] == 'c' && dest[1] == 'A'); showLeaks();
-	dest[0] = 'A';
 	/* 4 */ check(ft_memccpy(dest, "B", 0, 10) == dest + 2 && dest[0] == 'B' && dest[1] == 0); showLeaks();
 	char src[10];
 	memset(src, 0, 10);
