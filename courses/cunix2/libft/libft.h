517bb2d72de876fa0169b082b90cd5132c19f4af
FervorousGremlin <pobinderacc2021@gmail.com>
2021-08-16 12:25:36 +0300

    changes to Makefile

--- courses/cunix2/libft/libft.h
+++ courses/cunix2/libft/libft.h
@@ -12,3 +12,4 @@ void *ft_memccpy(void *dest, const void *src, int c, size_t n);
 char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strmap(char const *s, char (*f)(char));
 char * ft_strsub(char const *s, unsigned int start, size_t len);
+char *  ft_strtrim(char const *s);
