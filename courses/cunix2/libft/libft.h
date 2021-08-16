7e7776206564e1d31cf5ffb4f66edf5c2732d48e
vinegod <denchikchiter@gmail.com>
2021-08-16 22:40:31 +0300

    header changes

--- courses/cunix2/libft/libft.h
+++ courses/cunix2/libft/libft.h
@@ -6,7 +6,7 @@
  *        Created:  16.08.2021 14:01:48
  *       Compiler:  gcc
  *
- *         Author:  vinegod 
+ *         Author:  vinegod
  *
  * =====================================================================================
  */
@@ -17,8 +17,13 @@
 #include <stddef.h>
 
 void ft_bzero(void *s, size_t n);
-char* ft_strdup(char* str);
-int ft_strncmp(char* str1, char* str2, size_t num);
-
-
+char *ft_strdup(char *str);
+int ft_strncmp(char *str1, char *str2, size_t num);
+char *ft_strchr(const char *str, char c);
+char *ft_strrchr(char *str, char c);
+int ft_isalpha(int n);
+int ft_isdigit(int n);
+int ft_isascii(int n);
+int ft_toupper(int n);
+int ft_tolower(int n);
 #endif
