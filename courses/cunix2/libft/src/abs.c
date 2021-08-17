434d4c1a372df7eb7be34f9f60b5f6744f9f96bf
vinegod <denchikchiter@gmail.com>
2021-08-17 15:43:57 +0300

    add div.c abs.c

---
+++ courses/cunix2/libft/src/abs.c
@@ -0,0 +1,3 @@
+int ft_abs(int x) {
+    return  x >= 0 ? x : -x;
+}
---
+++ courses/cunix2/libft/src/div.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+div_t ft_div(int numer, int denom) {
+    div_t result;
+    result.quot = numer / denom;
+    result.rem = numer % denom;
+    return result;
+}
