98ed35b69bbab1dfd8bd655262ca6b7059fc77d2
vinegod <denchikchiter@gmail.com>
2021-08-17 16:42:53 +0300

    header changes

--- courses/cunix2/libft/libft.h
+++ courses/cunix2/libft/libft.h
@@ -15,6 +15,7 @@
 #define LIBFT_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 void ft_bzero(void *s, size_t n);
 char *ft_strdup(char *str);
@@ -26,5 +27,17 @@ int ft_isdigit(int n);
 int ft_isascii(int n);
 int ft_toupper(int n);
 int ft_tolower(int n);
+
+int ft_abs(int x);
+div_t ft_div(int numer, int denom);
+char *ft_strstr(char *haystack, char *needle);
+void *ft_memset(void *str, int c, size_t n);
+void *ft_memcpy(void *dest, const char *src, size_t n);
+void *ft_memccpy(void *dest, const void *src, int c, unsigned long int count);
+void *ft_memmove(void *dest, const char *src, unsigned long int n);
+void *ft_memchr(void *str, int c, unsigned long int n);
+int ft_memcmp ( const void * ptr1, const void * ptr2, unsigned long int num );
+
+
 
 #endif
