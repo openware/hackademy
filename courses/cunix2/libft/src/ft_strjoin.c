508621232b03920639ca152aa4f41b26ab288bf7
vinegod <denchikchiter@gmail.com>
2021-08-18 22:11:12 +0300

    add ft_strjoin.c

--- courses/cunix2/libft/src/ft_strjoin.c
+++ courses/cunix2/libft/src/ft_strjoin.c
@@ -11,14 +11,17 @@ unsigned int my_strlen(const char *restrict s)
     return len;
 }
 
-char *ft_strjoin(const char *s1, const char *s2) {
+char *ft_strjoin(char const *s1, char const *s2)
+{
     unsigned len1 = my_strlen(s1);
     unsigned len2 = my_strlen(s2);
     char *str = (char *)malloc(len1 + len2 + 1);
-    for (unsigned int i = 0; i < len1; i++) {
+    for (unsigned int i = 0; i < len1; i++)
+    {
         str[i] = s1[i];
     }
-    for (unsigned int i = 0; i < len2; i++) {
+    for (unsigned int i = 0; i < len2; i++)
+    {
         str[len1 + i] = s2[i];
     }
     str[len1 + len2] = '\0';
