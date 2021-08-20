7a6c0fc22d242267b0c1879f8e06a0c74d4b7ba5
actpohabtNS <actpohabt.ns@gmail.com>
2021-08-20 16:50:33 +0300

    add my_strlen implementation

---
+++ courses/cunix/ex01/src/my_strlen.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  my_strlen.c
+ *
+ *    Description: own implementation of std strel function 
+ *
+ *        Version:  1.0
+ *        Created:  08/20/2021 12:48:09 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Nikita Sazonov (ns), actpohabt.ns@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+unsigned int my_strlen(char *str)
+{
+    char *ch = str;
+    unsigned int len = 0;
+    while (*(ch++) != '\0')
+    {
+        len++;
+    }
+    return len;
+}
+/* -----  end of function my_strlen  ----- */
