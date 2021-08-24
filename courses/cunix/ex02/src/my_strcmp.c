cab0fa5ad2058a6e196243b43b498daeb1a14490
actpohabtNS <actpohabt.ns@gmail.com>
2021-08-24 21:41:18 +0300

    Fix brackets style

--- courses/cunix/ex02/src/my_strcmp.c
+++ courses/cunix/ex02/src/my_strcmp.c
@@ -17,7 +17,8 @@
  */
 
 
-int my_strcmp(char *str1, char *str2) {
+int my_strcmp(char *str1, char *str2)
+{
     while (*(str1) != '\0' && *(str2) != '\0') {
         if (*(str1) != *(str2)) {
             return *(str1) - *(str2);
