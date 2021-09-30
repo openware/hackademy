0880c5d969489ca89f7154635ac756e67398ee7a
pilot <vsvs2262asdfg@gmail.com>
2021-09-30 18:54:27 +0300

    strdup.c deleted node modules deleted

--- courses/cunix2/libft/src/ft_strdup.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-
-char *ft_strdup(char *src) {
-    char *dst = malloc(strlen (src) + 1);  // Space for length plus nul
-    if (dst == NULL) return NULL;          // No memory
-    strcpy(dst, src);                      // Copy the characters
-    return dst;                            // Return the new string
-}
