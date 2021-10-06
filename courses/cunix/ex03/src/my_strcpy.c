7ecd0bd08e3eacabc0f4f1c3ee517bc6c23c6628
olezhokmer2002 <kenzoyamazaki2002@gmail.com>
2021-10-06 21:25:20 +0300

    ex03 done

---
+++ courses/cunix/ex03/src/my_strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+char *my_strcpy(char *dest, const char *src){
+    if(sizeof(dest) >= sizeof(src)){
+        int i = 0;
+        while(src[i] != '\0')
+        {
+            dest[i] = src[i];
+            i++;
+        }
+        dest[i] = '\0';
+        return dest;
+    } else {
+        return 0;
+    }
+}
