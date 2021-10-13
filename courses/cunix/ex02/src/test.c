27e4c488a722284390e1aa843fbbb20b37a899e2
mhoncharuk <mhoncharuk@geeksforless.net>
2021-10-13 13:13:28 +0300

    Fixed tests for strcmp

--- courses/cunix/ex02/src/test.c
+++ courses/cunix/ex02/src/test.c
@@ -10,15 +10,15 @@ void   test_small()
   assert(my_strcmp("", "") == 0);
   assert(my_strcmp("A", "A") == 0);
   assert(my_strcmp("AB", "AB") == 0);
-  assert(my_strcmp("AB", "AC") == -1);
-  assert(my_strcmp("AB", "AA") == 1);
-  assert(my_strcmp("AB", "AD") == -1);
+  assert(my_strcmp("AB", "AC") < 0);
+  assert(my_strcmp("AB", "AA") > 0);
+  assert(my_strcmp("AB", "AD") < 0);
 }
 
 void   test_long()
 {
   assert(my_strcmp("HELLO WORLD", "HELLA WORLD") > 0);
-  assert(my_strcmp("HELLO WORLD", "HELL WORLD") == 1);
+  assert(my_strcmp("HELLO WORLD", "HELL WORLD") > 0);
 }
 
 int   main()
