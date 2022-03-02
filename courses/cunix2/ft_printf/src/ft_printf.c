f89d8e3c3dfcc6cfa2674515d2c9f818d310e299
Harpreet Singh <harpreet.7976@gmail.com>
2022-03-02 23:04:14 +0530

    final solution

--- courses/cunix2/ft_printf/src/ft_printf.c
+++ courses/cunix2/ft_printf/src/ft_printf.c
@@ -1,99 +1,155 @@
-#include <stdio.h>
-#include <stdlib.h>
+﻿#include <stdio.h>
 #include <stdarg.h>
-#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-char *_strrev (char *str)
+int find_length(const char *s)
 {
-  int i;
-  int len = 0;
-  char c;
-  if (!str)
-    return NULL;
-  while(str[len] != '\0'){
-    len++;
-  }
-  for(i = 0; i < (len/2); i++)
-  {
-    c = str[i];
-    str [i] = str[len - i - 1];
-    str[len - i - 1] = c;
-  }
-  return str;
+    int length = 0;
+    while (*s++)
+        length++;
+    return length;
 }
 
-char * _itoa(int i, char *strout, int base)
+char *to_string(long int nmb, int *len)
 {
-  char *str = strout;
-  int digit, sign = 0;
-  if (i < 0) {
-    sign = 1;
-    i *= -1;
-  }
-  while(i) {
-    digit = i % base;
-    *str = (digit > 9) ? ('A' + digit - 10) : '0' + digit;
-    i = i / base;
-    str ++;
-  }
-  if(sign) {
-  *str++ = '-';
-  }
-  *str = '\0';
-  _strrev(strout);
-  return strout;
-}
+    int sign;
+    if ((sign = nmb) < 0)
+        nmb = -nmb;
+    *len = 0;
+    long num = nmb;
+    do
+    {
+        *len = *len + 1;
+    } while ((num /= 10) > 0);
 
-int ft_printf (char * str, ...)
-{
-  va_list vl;
-  int i = 0, j=0;
-  char buff[100]={0}, tmp[20];
-  char * str_arg;
- 
-  va_start( vl, str );
-  while (str && str[i])
-  {
-    if(str[i] == '%'){
-      i++;
-      switch (str[i]) {
-        case 'c': {
-          buff[j] = (char)va_arg( vl, int );
-          j++;
-          break;
-        }
-        case 'd': {
-          _itoa(va_arg( vl, int ), tmp, 10);
-          strcpy(&buff[j], tmp);
-          j += strlen(tmp);
-          break;
-        }
-        case 'x': {
-          _itoa(va_arg( vl, int ), tmp, 16);
-          strcpy(&buff[j], tmp);
-          j += strlen(tmp);
-          break;
-        }
-        case 'o': {
-          _itoa(va_arg( vl, int ), tmp, 8);
-          strcpy(&buff[j], tmp);
-          j += strlen(tmp);
-          break;
-        }
-        case 's': {
-          str_arg = va_arg( vl, char* );
-          strcpy(&buff[j], str_arg);
-          j += strlen(str_arg);
-          break;
-        }
-      }
-    } else {
-      buff[j] =str[i];
-      j++;
+    char *result = (char *)malloc(*len + 1);
+
+    for (int i = 0; i < *len; i++)
+    {
+        result[*len - 1 - i] = nmb % 10 + '0';
+        nmb /= 10;
     }
-    i++;
-  }
-  fwrite(buff, j, 1, stdout);
-  va_end(vl);
-  return j;
+    result[*len] = '\0';
+    return result;
+}
+
+void fill_buffer(char *c, int number)
+{
+    while (number > 0)
+    {
+        write(1, c, 1);
+        number--;
+    }
+}
+
+void print_out(const char *str)
+{
+    for (; *str; str++)
+    {
+        write(1, str, 1);
+    }
+}
+void print_begin(char show_sign, char print_space)
+{
+    if (show_sign == '\0')
+    {
+        if (print_space)
+        {
+            char *s = &print_space;
+            write(1, s, 1);
+        }
+    }
+    else
+    {
+        char *s = &show_sign;
+        write(1, s, 1);
+    }
+}
+
+void print(const char *str, char left, char fill, char show_sign, char print_space, int num)
+{
+    int first_char = (show_sign || print_space) ? 1 : 0;
+    if (left)
+    {
+        print_begin(show_sign, print_space);
+        print_out(str);
+        fill_buffer(&fill, num - first_char);
+    }
+    else
+    {
+
+        if (fill == '0')
+        {
+            print_begin(show_sign, print_space);
+            fill_buffer(&fill, num - first_char);
+        }
+        else
+        {
+            fill_buffer(&fill, num - first_char);
+            print_begin(show_sign, print_space);
+        }
+        _print_out(str);
+    }
+}
+
+int ft_printf(const char *format, ...)
+{
+    va_list ap;
+    va_start(ap, format);
+    const char *p;
+
+    for (p = format; *p; p++)
+    {
+        if (*p != '%')
+        {
+            write(1, p, 1);
+            continue;
+        }
+
+        char _print_space = *++p == ' ' ? ' ' : '\0';
+
+        while (*p == ' ')
+            p++;
+
+        char show_sign = *p == '+' ? *p++ : '\0';
+        char left = *p == '-' ? *p++ : '\0';
+        char fill = *p == '0' ? '0' : ' ';
+
+        int order = 0;
+        for (; *p >= '0' && *p <= '9'; p++)
+            order = order * 10 + *p - '0';
+
+        switch (*p)
+        {
+        case 'i':
+        case 'd': {
+            int len;
+            int num = va_arg(ap, int);
+            char *value = to_string(num, &len);
+            show_sign = num < 0 ? '-' : show_sign;
+            print(value, left, fill, show_sign, _print_space, order - len);
+            free(value);
+        }
+        break;
+        case 'c': {
+            char value = va_arg(ap, int);
+            char *s = &value;
+            print(s, left, fill, show_sign, _print_space, order - 1);
+        }
+        break;
+        case 's': {
+            char *value = va_arg(ap, char *);
+            if (value == NULL)
+                value = "(null)";
+            print(value, left, fill, show_sign, _print_space, order - find_length(value));
+        }
+        break;
+        default:
+            write(1, p, 1);
+            break;
+        }
+    }
+    va_end(ap);
+    return p - format;
 }
